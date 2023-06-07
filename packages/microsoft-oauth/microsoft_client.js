20fe27d1fc90ec928b33745e6f2844fefd995b9a
Minh Nguyen <minhna@gmail.com>
2023-06-08 00:23:41 +0700

    update client login url

--- packages/microsoft-oauth/microsoft_client.js
+++ packages/microsoft-oauth/microsoft_client.js
@@ -22,14 +22,44 @@ Microsoft.requestCredential = (options, credentialRequestCompleteCallback) => {
   }
   const credentialToken = Random.secret();
 
+  // we need the email scope to get user id from google.
+  const requiredScopes = {
+    openid: 1,
+    "https://graph.microsoft.com/.default": 1,
+  };
+  let scopes = options.requestPermissions || ["openid"];
+  scopes.forEach((scope) => (requiredScopes[scope] = 1));
+  scopes = Object.keys(requiredScopes);
+
+  const loginUrlParameters = {};
+  if (config.loginUrlParameters) {
+    Object.assign(loginUrlParameters, config.loginUrlParameters);
+  }
+  if (options.loginUrlParameters) {
+    Object.assign(loginUrlParameters, options.loginUrlParameters);
+  }
+
   const loginStyle = OAuth._loginStyle("microsoft", config, options);
 
+  Object.assign(loginUrlParameters, {
+    response_type: "id_token+token",
+    client_id: config.clientId,
+    scope: scopes.join(" "), // space delimited
+    redirect_uri: OAuth._redirectUri("microsoft", config),
+    response_mode: "fragment",
+    state: OAuth._stateParam(loginStyle, credentialToken, options.redirectUrl),
+    nonce: credentialToken,
+  });
   const loginUrl =
-    `https://login.microsoftonline.com/${config.tenantId}/oauth2/v2.0/token` +
-    `?client_id=${config.clientId}` +
-    `?client_secret=${config.secret}` +
-    `&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default` +
-    `&grant_type=client_credentials`;
+    "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+    Object.keys(loginUrlParameters)
+      .map(
+        (param) =>
+          `${encodeURIComponent(param)}=${encodeURIComponent(
+            loginUrlParameters[param]
+          )}`
+      )
+      .join("&");
 
   OAuth.launchLogin({
     loginService: "microsoft",
