415be9d463c9c8f72e3e827c1805418595026507
Minh Nguyen <minhna@gmail.com>
2023-06-09 15:05:26 +0700

    remove tenant id field from configure fields

--- packages/microsoft-config-ui/microsoft_configure.js
+++ packages/microsoft-config-ui/microsoft_configure.js
@@ -3,7 +3,6 @@ Template.configureLoginServiceDialogForMicrosoft.helpers({
 });
 
 Template.configureLoginServiceDialogForMicrosoft.fields = () => [
-  { property: "tenantId", label: "Tenant ID" },
   { property: "clientId", label: "Client ID" },
   { property: "secret", label: "Client Secret" },
 ];
